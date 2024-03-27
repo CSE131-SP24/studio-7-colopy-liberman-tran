f67aa7bb523f2f94c95b7172b55acac468afc706
xiomaracolopy <xiomaracolopy@localhost>
2024-03-27 13:50:11 -0500

    done

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -7,13 +7,21 @@ public class Die {
 	private Die(int initSides) {
 		sides = initSides;
 	}
-	
+	/**
+	 * Simulates a die roll
+	 * 
+	 * @param sides
+	 * @return
+	 */
 	public int rollDie(int sides) {
 		int rollResult = (int)(Math.random() * sides + 1);
 		
 		return rollResult;
 		
 	}
+	/**
+	 * Converts everything to String
+	 */
 	public String toString() {
 		String string = "Number of sides: " + this.sides + ", Roll result: " + rollDie(sides);
 		
